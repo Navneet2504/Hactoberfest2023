24c7a09f921937565de7ead443ca65c39b9d567d
Navneet2504 <113124774+Navneet2504@users.noreply.github.com>
2023-10-07 18:30:18 +0530

    Create myprogram.java

---
+++ myprogram.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class first{
+    public static void main(String arg[]){
+        System.out.println("helloworld");
+        Scanner sc=new Scanner(System.in);
+        int name1 =sc.nextInt();
+        int name2 =sc.nextInt();
+        System.out.println(name1+name2);
+        int age =sc.nextInt();
+        if(age>=18){
+            System.out.println("YOU ARE ELIGIBLE FOE Voting");
+        }
+        else{
+            System.out.println("NOT ELIGIBLE FOR Votting");
+        }
+        for(int i=0;i<name1;i++){
+            System.out.println("NAVNEET SAVITA");
+        }
+        
+    }
+} 
